c6d8cc00fe8075da725eb2e0a20bfd2bce6d84ce
akina <akinayturan@gmail.com>
2017-01-15 00:05:05 +0300

    style edit 3

---
+++ application/views/buy.php
@@ -0,0 +1,20 @@
+<div class="portlet light bordered">
+    <div class="portlet-title">
+        <div class="caption">
+            <i class="fa fa-hand-peace-o font-blue-sharp"></i>
+            <span class="caption-subject font-blue-sharp bold uppercase">Buy Credits</span>
+        </div>
+    </div>
+    <div class="portlet-body">
+        <div class="row">
+            <div class="col-md-12">
+                <p>If the viewer does not generate enough credits for your needs, you can increase the number of visits
+                    received per day on your site by purchasing additional credits. We offer several convenient payment
+                    options. SMS (Allopass), or by debit or credit card (Paypal). You can choose the currency - Euro or
+                    Dollars (US) - whichever suits you best.</p>
+
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ application/views/earn.php
@@ -0,0 +1,77 @@
+<div class="portlet light bordered">
+    <div class="portlet-title">
+        <div class="caption">
+            <i class="fa fa-hand-peace-o font-blue-sharp"></i>
+            <span class="caption-subject font-blue-sharp bold uppercase">Earn Credits</span>
+        </div>
+    </div>
+    <div class="portlet-body">
+        <div class="row">
+            <div class="col-md-12">
+                <p>
+                    To earn credits, you must visit the sites of other members through the viewer. Whenever you visit
+                    another member site, you will earn 0.8 credits each 15 seconds redeemable against a visit to your
+                    site. Each credit earned will be deposited into your bank unless you have selected auto distribution
+                    option. To start the Viewer, click below.
+
+                </p>
+                <button class="btn blue btn-block">Start Viewer</button>
+<br />
+                <table class="table table-hover table-striped table-bordered">
+                    <tbody>
+
+                    <tr>
+                        <td>Auto distribution</td>
+                        <td>
+                            1223
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Light Viewer Link</td>
+                        <td>
+                            sss
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <hr/>
+        <div class="row">
+            <div class="col-md-12">
+                <p class="font-red-intense">Multiple Viewers </p>
+                <p>
+                    To use Multiple Viewers, each viewers must have their own IP adresse. In order to have Multiple IPs you can use VPS.
+                </p>
+
+                <p class="font-red-intense">Active Viewers </p>
+                <table class="table table-hover table-striped table-bordered">
+                    <tbody>
+                    <tr>
+                        <th>
+                            IP Address
+                        </th>
+                        <th>
+                            Delay
+                        </th>
+                        <th>
+                            Last Url
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>88.241.39.87 <br> TURKEY</td>
+                        <td>
+                            9 sec
+                        </td>
+                        <td>
+                            http://www.hamilton-radio.com <br>
+                            Type: Classic Windows Chrome 55.0.2883.87
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
