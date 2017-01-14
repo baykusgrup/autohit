8fa6cd19af7951de17d0e82fdbcfb0348be6179d
akina <akinayturan@gmail.com>
2017-01-14 21:33:30 +0300

    my sites and distrubituon , blocked sites

---
+++ application/views/blocked.php
@@ -0,0 +1,37 @@
+<div class="portlet light bordered">
+    <div class="portlet-title">
+        <div class="caption">
+            <i class="fa fa-hand-peace-o font-blue-sharp"></i>
+            <span class="caption-subject font-blue-sharp bold uppercase">List of blocked websites</span>
+        </div>
+    </div>
+    <div class="portlet-body">
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-hover table-striped table-bordered">
+                    <tbody>
+                    <tr>
+                        <th>#</th>
+                        <th>
+                            URL
+                        </th>
+                        <th>
+                            -
+                        </th>
+                    </tr>
+                    <tr>
+                        <td> 1</td>
+                        <td>
+                            http://www.meinsuperjob.de/?af=58
+                        </td>
+                        <td>
+                            sss
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ application/views/sites.php
@@ -0,0 +1,61 @@
+<div class="portlet light bordered">
+    <div class="portlet-title">
+        <div class="caption">
+            <i class="fa fa-hand-peace-o font-blue-sharp"></i>
+            <span class="caption-subject font-blue-sharp bold uppercase">My Sites</span>
+        </div>
+    </div>
+    <div class="portlet-body">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-info">Easy Distribution</p>
+
+                <table class="table table-hover table-striped table-bordered">
+                    <tbody>
+                    <tr>
+                        <td colspan="2"> Easy Distribution - <a class="font-red-mint"> Available Credits: 2222</a></td>
+                    </tr>
+                    <tr>
+                        <td> Total Amount Credit</td>
+                        <td>
+                            sss
+                        </td>
+                    </tr>
+                    <tr>
+                        <td> Per Site Amount Credit</td>
+                        <td>sss
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                <hr/>
+                <p class="text-info">Active website</p>
+
+                <table class="table table-hover table-striped table-bordered">
+                    <tbody>
+                    <tr>
+                    <th>Site name</th>
+                    <th>Url</th>
+                    <th>Credits</th>
+                    <th>Received today</th>
+                    </tr>
+                    <tr>
+                        <td>(Aucun)</td>
+                        <td>http://baykusgrup.com/?rastgele</td>
+                        <td>11.299</td>
+                        <td>Total Amount Credit</td>
+
+
+                    </tr>
+                    </tbody>
+                </table>
+
+                <hr />
+
+                <p class="text-info">Site Management</p>
+
+            </div>
+        </div>
+
+    </div>
+</div>
