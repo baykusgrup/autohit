ee70663a0aa99b7e5e484201d45890498d45545a
akina <akinayturan@gmail.com>
2017-01-17 08:56:53 +0300

    register sidebar kapatıldı ve gizlendi

--- application/views/_head.php
+++ application/views/_head.php
@@ -101,7 +101,9 @@
 <?php
 if (class_exists('Login')) {
     echo "<body class=\"login\" style=\" background-color: #fff;\">";
-} else {
+} else if (class_exists('Register')) {
+    echo "<body class=\"page-header-fixed page-sidebar-closed page-content-white page-sidebar-fixed  page-boxed page-md\">";
+}else {
     echo "<body class=\"page-header-fixed page-sidebar-opened page-content-white page-sidebar-fixed  page-boxed page-md\">";
 }
 ?>
@@ -216,99 +218,106 @@ if (!class_exists('Login')) {
     <div class="container">
         <!-- BEGIN CONTAINER -->
         <div class="page-container">
-            <!-- BEGIN SIDEBAR -->
-            <div class="page-sidebar-wrapper">
+
+            <?php
+            if (!class_exists('Register')) {
+                ?>
                 <!-- BEGIN SIDEBAR -->
-                <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
-                <!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
-                <div class="page-sidebar navbar-collapse collapse">
-                    <!-- BEGIN SIDEBAR MENU -->
-                    <!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
-                    <!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
-                    <!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
+                <div class="page-sidebar-wrapper">
+                    <!-- BEGIN SIDEBAR -->
                     <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
-                    <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
-                    <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
-                    <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false" data-auto-scroll="true"
-                        data-slide-speed="200" style="padding-top: 20px">
-                        <!-- DOC: To remove the sidebar toggler from the sidebar you just need to completely remove the below "sidebar-toggler-wrapper" LI element -->
-                        <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
-                        <li class="sidebar-toggler-wrapper hide">
-                            <div class="sidebar-toggler">
-                                <span></span>
-                            </div>
-                        </li>
-                        <!-- END SIDEBAR TOGGLER BUTTON -->
+                    <!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+                    <div class="page-sidebar navbar-collapse collapse">
+                        <!-- BEGIN SIDEBAR MENU -->
+                        <!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
+                        <!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
+                        <!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
+                        <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+                        <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
+                        <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+                        <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false" data-auto-scroll="true"
+                            data-slide-speed="200" style="padding-top: 20px">
+                            <!-- DOC: To remove the sidebar toggler from the sidebar you just need to completely remove the below "sidebar-toggler-wrapper" LI element -->
+                            <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+                            <li class="sidebar-toggler-wrapper hide">
+                                <div class="sidebar-toggler">
+                                    <span></span>
+                                </div>
+                            </li>
+                            <!-- END SIDEBAR TOGGLER BUTTON -->
 
-                        <li class="nav-item start ">
-                            <a href="<?php echo base_url() ?>index.php/Account" class="nav-link nav-toggle">
-                                <i class="icon-home"></i>
-                                <span class="title">My Account</span>
-                            </a>
-                        </li>
-                        <li class="heading">
-                            <h3 class="uppercase">About Site</h3>
-                        </li>
-                        <li class="nav-item  ">
-                            <a href="<?php echo base_url() ?>index.php/Earn" class="nav-link nav-toggle">
-                                <i class="icon-settings"></i>
-                                <span class="title">Earn Credits</span>
-                            </a>
-                        </li>
-                        <li class="nav-item  ">
-                            <a href="<?php echo base_url() ?>index.php/Sites" class="nav-link nav-toggle">
-                                <i class="icon-diamond"></i>
-                                <span class="title">My Sites</span>
-                            </a>
-                        </li>
-                        <li class="nav-item  ">
-                            <a href="<?php echo base_url() ?>index.php/Blocked" class="nav-link nav-toggle">
-                                <i class="icon-dislike"></i>
-                                <span class="title">Blocked Sites</span>
-                            </a>
-                        </li>
+                            <li class="nav-item start ">
+                                <a href="<?php echo base_url() ?>index.php/Account" class="nav-link nav-toggle">
+                                    <i class="icon-home"></i>
+                                    <span class="title">My Account</span>
+                                </a>
+                            </li>
+                            <li class="heading">
+                                <h3 class="uppercase">About Site</h3>
+                            </li>
+                            <li class="nav-item  ">
+                                <a href="<?php echo base_url() ?>index.php/Earn" class="nav-link nav-toggle">
+                                    <i class="icon-settings"></i>
+                                    <span class="title">Earn Credits</span>
+                                </a>
+                            </li>
+                            <li class="nav-item  ">
+                                <a href="<?php echo base_url() ?>index.php/Sites" class="nav-link nav-toggle">
+                                    <i class="icon-diamond"></i>
+                                    <span class="title">My Sites</span>
+                                </a>
+                            </li>
+                            <li class="nav-item  ">
+                                <a href="<?php echo base_url() ?>index.php/Blocked" class="nav-link nav-toggle">
+                                    <i class="icon-dislike"></i>
+                                    <span class="title">Blocked Sites</span>
+                                </a>
+                            </li>
 
-                        <li class="heading">
-                            <h3 class="uppercase">Other Thinks</h3>
-                        </li>
-                        <li class="nav-item  ">
-                            <a href="<?php echo base_url() ?>index.php/Buy" class="nav-link nav-toggle">
-                                <i class="icon-wallet"></i>
-                                <span class="title">Buy Credits</span>
-                            </a>
-                        </li>
-                        <li class="nav-item  ">
-                            <a href="<?php echo base_url() ?>index.php/Refferal" class="nav-link nav-toggle">
-                                <i class="icon-users"></i>
-                                <span class="title">Refferal</span>
-                            </a>
-                        </li>
-                        <li class="nav-item  ">
-                            <a href="<?php echo base_url() ?>index.php/Statistics" class="nav-link nav-toggle">
-                                <i class="icon-bar-chart"></i>
-                                <span class="title">Statistics</span>
-                            </a>
-                        </li>
-                        <li class="nav-item  ">
-                            <a href="<?php echo base_url() ?>index.php/Help" class="nav-link nav-toggle">
-                                <i class="icon-directions"></i>
-                                <span class="title">Help</span>
-                            </a>
-                        </li>
-                        <li class="nav-item  ">
-                            <a href="mailto:info@autohit.com?subject=#sss Comments&body=Hi!"
-                               class="nav-link nav-toggle">
-                                <i class="icon-envelope-letter"></i>
-                                <span class="title">Contact us</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- END SIDEBAR MENU -->
-                    <!-- END SIDEBAR MENU -->
+                            <li class="heading">
+                                <h3 class="uppercase">Other Thinks</h3>
+                            </li>
+                            <li class="nav-item  ">
+                                <a href="<?php echo base_url() ?>index.php/Buy" class="nav-link nav-toggle">
+                                    <i class="icon-wallet"></i>
+                                    <span class="title">Buy Credits</span>
+                                </a>
+                            </li>
+                            <li class="nav-item  ">
+                                <a href="<?php echo base_url() ?>index.php/Refferal" class="nav-link nav-toggle">
+                                    <i class="icon-users"></i>
+                                    <span class="title">Refferal</span>
+                                </a>
+                            </li>
+                            <li class="nav-item  ">
+                                <a href="<?php echo base_url() ?>index.php/Statistics" class="nav-link nav-toggle">
+                                    <i class="icon-bar-chart"></i>
+                                    <span class="title">Statistics</span>
+                                </a>
+                            </li>
+                            <li class="nav-item  ">
+                                <a href="<?php echo base_url() ?>index.php/Help" class="nav-link nav-toggle">
+                                    <i class="icon-directions"></i>
+                                    <span class="title">Help</span>
+                                </a>
+                            </li>
+                            <li class="nav-item  ">
+                                <a href="mailto:info@autohit.com?subject=#sss Comments&body=Hi!"
+                                   class="nav-link nav-toggle">
+                                    <i class="icon-envelope-letter"></i>
+                                    <span class="title">Contact us</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <!-- END SIDEBAR MENU -->
+                        <!-- END SIDEBAR MENU -->
+                    </div>
+                    <!-- END SIDEBAR -->
                 </div>
                 <!-- END SIDEBAR -->
-            </div>
-            <!-- END SIDEBAR -->
+                <?php
+            }
+            ?>
             <!-- BEGIN CONTENT -->
             <div class="page-content-wrapper">
                 <!-- BEGIN CONTENT BODY -->
