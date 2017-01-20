706d9334d7361201883a95c7c6efd99dfd9fc74a
akina <akinayturan@gmail.com>
2017-01-20 21:31:15 +0300

    referal

--- application/views/refferal.php
+++ application/views/refferal.php
@@ -11,16 +11,23 @@
                 <p class="font-red-intense">Sponsorship </p>
                 <p>Here's your referral link to share:</p>
 
-                <p><strong>https://www.websyndic.com/?ref=881606</strong></p>
+                <p><strong> <?php
+                        if (isset($userInfo[0]["referance_code"])) {
+                            echo "<a href=\"" . base_url() . "index.php/Register/?ref=" . $userInfo[0]["referance_code"] . "\" >" . base_url() . "index.php/Register/?ref=" . $userInfo[0]["referance_code"] . "</a>";
+                        } ?></strong></p>
 
                 <p>Or, if you prefer, you can add this banner on your site:</p>
+                <textarea style="margin-bottom: 10px" class="col-md-12" rows="5"> <!--Autosurf NearlyWeb Banner--><a
+                            href='<?php if (isset($userInfo[0]['referance_code'])) {
+                                echo base_url() . "index.php/Register/?ref=" . $userInfo[0]["referance_code"];
+                            } ?>' title='autosurf' alt='autosurf'><img
+                                src='<?php echo base_url(); ?>/banners/ban2.gif' width='468' height='60' border='0'
+                                title='autosurf' alt='autosurf'></a><!--Autosurf NearlyWeb Banner--> </textarea>
 
-                <textarea style="margin-bottom: 10px" class="col-md-12"> <!--Autosurf Websyndic Banner--><a href='https://www.Websyndic.com/?ref=881606' title='autosurf' alt='autosurf'><img src='https://websyndic.com/banners/ban2.gif' width='468' height='60' border='0' title='autosurf' alt='autosurf'></a><!--Autosurf Websyndic Banner--> </textarea>
 
-
-                <p>You earn 10% of all credits that members registered through your referral link will earn using the viewer.</p>
+                <p>You earn 10% of all credits that members registered through your referral link will earn using the
+                    viewer.</p>
                 <p>Below is the list of members and the number of credits you have earned.</p>
-
 
 
                 <table class="table table-hover table-striped table-bordered">
