6e3496d32ffc8f4247ac9070c88a6d8733119ae8
akina <akinayturan@gmail.com>
2017-02-09 17:38:16 +0300

    login kontrol

--- application/views/_head.php
+++ application/views/_head.php
@@ -164,7 +164,7 @@
                     </li>
 
                     <?php
-                    if (class_exists('Register') || class_exists('Home') || class_exists('Login')) {
+                    if ($this->session->userdata('user_id') == NULL) {
                         ?>
                         <li class="dropdown dropdown-user">
                             <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"
@@ -191,7 +191,6 @@
                         <?php
                     } else {
                         ?>
-
 
                         <!-- BEGIN USER LOGIN DROPDOWN -->
                         <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
@@ -236,21 +235,13 @@
         <div class="page-container">
 
             <?php
-            if (class_exists('Register') || class_exists('Home') || class_exists('Login')) {
+            if ($this->session->userdata('user_id') == NULL) {
                 ?>
                 <!-- BEGIN SIDEBAR -->
                 <div class="page-sidebar-wrapper">
                     <!-- BEGIN SIDEBAR -->
-                    <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
-                    <!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
                     <div class="page-sidebar navbar-collapse collapse">
                         <!-- BEGIN SIDEBAR MENU -->
-                        <!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
-                        <!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
-                        <!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
-                        <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
-                        <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
-                        <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
                         <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false"
                             data-auto-scroll="true"
                             data-slide-speed="200" style="padding-top: 20px">
@@ -328,21 +319,10 @@
                 <!-- BEGIN SIDEBAR -->
                 <div class="page-sidebar-wrapper">
                     <!-- BEGIN SIDEBAR -->
-                    <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
-                    <!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
                     <div class="page-sidebar navbar-collapse collapse">
-                        <!-- BEGIN SIDEBAR MENU -->
-                        <!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
-                        <!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
-                        <!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
-                        <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
-                        <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
-                        <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
                         <ul class="page-sidebar-menu  page-header-fixed " data-keep-expanded="false"
                             data-auto-scroll="true"
                             data-slide-speed="200" style="padding-top: 20px">
-                            <!-- DOC: To remove the sidebar toggler from the sidebar you just need to completely remove the below "sidebar-toggler-wrapper" LI element -->
-                            <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
                             <li class="sidebar-toggler-wrapper hide">
                                 <div class="sidebar-toggler">
                                     <span></span>
@@ -351,8 +331,14 @@
                             <!-- END SIDEBAR TOGGLER BUTTON -->
 
                             <li class="nav-item start ">
-                                <a href="<?php echo base_url() ?>index.php/Account" class="nav-link nav-toggle">
+                                <a href="<?php echo base_url() ?>" class="nav-link nav-toggle">
                                     <i class="icon-home"></i>
+                                    <span class="title"><?php echo lang("HomePage"); ?></span>
+                                </a>
+                            </li>
+                            <li class="nav-item start ">
+                                <a href="<?php echo base_url() ?>index.php/Account" class="nav-link nav-toggle">
+                                    <i class="icon-user"></i>
                                     <span class="title"><?php echo lang("MyAccount"); ?></span>
                                 </a>
                             </li>
@@ -414,7 +400,6 @@
                             </li>
 
                         </ul>
-                        <!-- END SIDEBAR MENU -->
                         <!-- END SIDEBAR MENU -->
                     </div>
                     <!-- END SIDEBAR -->
